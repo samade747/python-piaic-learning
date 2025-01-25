61900c73e750eaeae2ff81505db544131f8d8a40
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-01-25 15:11:42 +0500

    Create setdata.py

---
+++ setdata.py
@@ -0,0 +1,16 @@
+# setdata type
+
+my_set = {1, 2, 3, 4, 5}
+
+print(my_set)  # Output: {1, 2, 3, 4, 5}
+
+my_set.add(6)
+print(my_set)  # Output: {1, 2, 3, 4, 5, 6} 
+
+print(my_set == {1, 2, 3, 4, 5, 6})  # Output: True
+
+my_set = {1, 2, 3, 4, 5}
+try 
+    my_set[0] = 10
+except TypeError as e:
+    print(e)  # Output: 'set' object does not support item assignment
