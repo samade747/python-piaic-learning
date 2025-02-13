7705dc2211dadfe84cd94731af7d52414454a689
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-02-13 15:19:23 +0500

    Update classvariables.py

--- data/Full/classvaribalesinpython/classvariables.py
+++ data/Full/classvaribalesinpython/classvariables.py
@@ -2,3 +2,16 @@
                     #define outside of any methods in the class
                     #allow you to share data amond all objects created from that class
 
+class Student:
+    # class variable
+
+    class_year = 2025
+    num_studets = 0
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        Student.num_studets += 1
+
+    student1 = Student("samad", 28) # type: ignore
+    student2 = Student("mary", 22) # type: ignore
