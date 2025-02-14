d362e610b9d97b25fa1a362fc94d6a6fb16f9aab
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-02-14 15:34:24 +0500

    Update multipleOrmutilevelinhertance.py

--- data/Full/multipleOrmutilevelinhertance/multipleOrmutilevelinhertance.py
+++ data/Full/multipleOrmutilevelinhertance/multipleOrmutilevelinhertance.py
@@ -3,6 +3,32 @@
 
 # mutlilevel inheritance = inherit from a class that inherits from another class
                             # C(B) <- B(A) <- A
+class Animal:
+    def eat(self):
+        print('The animal is eating')
+
+class Prey(Animal):
+    def flee(self):
+        print('The animal is fleeing')
 
 
- 
+class Predator(Animal):
+    def hunt(self):
+        print('The animal is hunting')
+
+
+class Rabbit(Prey):
+    pass
+
+
+class Hawk(Predator):
+    pass
+
+class Fish(Prey, Predator):
+    pass
+
+rabbit = Rabbit()
+hawk = Hawk()
+fish = Fish()
+
+hawk.flee()
