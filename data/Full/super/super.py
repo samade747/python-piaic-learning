ec58bb4c1a3ee27750cb03615f55f049c09f03e5
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-02-15 10:53:33 +0500

    Update super.py

---
+++ data/Full/super/super.py
@@ -0,0 +1,47 @@
+# super() = Funtion that allows you to call a method from the parent class.
+# allows you to extend the functionality of the inherited method.
+
+
+# super class
+class Shape:
+    def __init__(self, color, is_filled):
+        self.color = color
+        self.is_filled = is_filled
+
+    def describe(self):
+        print(f"Shape: {self.color}, {self.is_filled}")
+
+class Circle(Shape):
+    def __init__(self, color, is_filled, radius):
+        super().__init__(color, is_filled)
+        self.radius = radius
+
+class Square(Shape):
+    def __init__(self, color, is_filled, width):
+        super().__init__(color, is_filled)
+        self.width = width
+
+class Triangle(Shape):
+    def __init__(self, color, is_filled, width, height):
+        super().__init__(color, is_filled)
+        self.width = width
+        self.height = height
+
+
+Circle = Circle("red", True, 5)
+Square = Square("blue", False, 10)
+Triangle = Triangle("green", True, 15, 20)
+
+print(f"Circle: {Circle.color}, {Circle.is_filled}, {Circle.radius}")
+print(f"Square: {Square.color}, {Square.is_filled}, {Square.width}")
+print(f"Triangle: {Triangle.color}, {Triangle.is_filled}, {Triangle.width}, {Triangle.height}")
+
+Circle.describe()
+Square.describe()
+Triangle.describe()
+
+
+
+
+
+ 
