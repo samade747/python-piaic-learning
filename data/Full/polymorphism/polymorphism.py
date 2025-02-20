05fd21006cf6731cc4c12ca65f784a4b24128e2a
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-02-20 15:09:28 +0500

    Update polymorphism.py

--- data/Full/polymorphism/polymorphism.py
+++ data/Full/polymorphism/polymorphism.py
@@ -6,5 +6,35 @@
 # 1. inheritance = an object could be treated of the same type as a parent class
 # 2. Duck typing = object must have neccsary attributes / methods
 
+from abc import ABC, abstractmethod
 
+class Shape:
+   
+   @abstractmethod
+   def area(self):
+        pass
 
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius ** 2
+
+    
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side ** 2
+
+class Triangle(Shape):
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return 0.5 * self.base * self.height
+
+shapes = [Circle(7), Square(5), Triangle(3, 6)]
