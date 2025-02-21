8262b4bf165a582fb9227a0b9a1ddbd244c19afb
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-02-22 03:52:47 +0500

    Update if.py

---
+++ data/Full/if&else/if.py
@@ -0,0 +1,16 @@
+# if = Do some only if ssome condifion is true
+# else do something else
+
+
+# age = int(input("Enter your age: "))
+
+# if age >= 18:
+#     print("You are an adult")
+#     print("You can vote")
+# elif age < 0 :
+#     print("You haven't been born yet")
+# else:
+#     print("You are a child")
+ 
+
+
