2c34c5164d08ab8f12fce0ae0f18feec103732e6
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-02-28 22:01:48 +0500

    as

--- data/Full/Ducktyping/ducktyping.py
+++ data/Full/Ducktyping/ducktyping.py
@@ -1,20 +1,62 @@
-#Duck typing in pyhton = another way to achieve polymorphism besides inheritance
-# object must have the minimun neccesary attriubtues /methods
+# Duck typing in Python is another way to achieve polymorphism besides inheritance.
+# It allows objects to be used interchangeably if they have the required attributes/methods.
 
+# Base class
 class Animal:
-    alive = True
+    alive = True  # Class attribute indicating if the object is alive
 
+# Dog class inherits from Animal
 class Dog(Animal):
-    def speak(self):
+    def speak(self):  # Method to make a sound
         print("Woof woof")
 
+# Cat class inherits from Animal
 class Cat(Animal):
-    def speak(self):
+    def speak(self):  # Method to make a sound
         print("Meow")
 
-animals = [Dog(), Cat()]:
+# Car class does NOT inherit from Animal but has the same method and attribute
+class Car:
+    alive = False  # Class attribute indicating it's not alive
+    
+    def speak(self):  # Method to make a sound
+        print("Honk honk")
 
+# Creating a list of different objects that have a 'speak' method
+animals = [Dog(), Cat(), Car()]  # Removed colon (syntax error)
+
+# Iterating through the list and calling the 'speak' method on each object
 for animal in animals:
-    animal.speak()
-    print(animal.alive)
+    animal.speak()  # Calls the respective 'speak' method
+    print(animal.alive)  # Prints whether the object is alive or not
+
+
+# #Duck typing in pyhton = another way to achieve polymorphism besides inheritance
+# # object must have the minimun neccesary attriubtues /methods
+
+# class Animal:
+#     alive = True
+
+# class Dog(Animal):
+#     def speak(self):
+#         print("Woof woof")
+
+# class Cat(Animal):
+#     def speak(self):
+#         print("Meow")
+
+
+# class Car:
+
+#     alive = False
+
+#     def speak(self):
+#         print("Honk honk")
+
+
+# animals = [Dog(), Cat(), Car()]:
+
+# for animal in animals:
+#     animal.speak()
+#     print(animal.alive)
 
