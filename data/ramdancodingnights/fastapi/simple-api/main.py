7fd4269af932c979de1b7531c1c0b73a43e222df
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-03-07 04:05:02 +0500

    0404

---
+++ data/ramdancodingnights/fastapi/simple-api/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+import random
+
+
+app = FastAPI() 
+
+
+side_hustles = [
+    "Freelancing -  start offering your skils online",
+    "Dropshipping - sell without handling & handling inventory "
+    "stock market - "
+
+
+
+]
