6f66a6e073a307863a5f775a1cce1f5357e24104
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-03-22 17:46:32 +0500

    546

---
+++ data/homework_projects/02_agreement_bot.md/agg.py
@@ -0,0 +1,5 @@
+# Ask the user for their favorite animal
+favorite_animal = input("What's your favorite animal? ")
+
+# Respond with the user's favorite animal
+print(f"My favorite animal is also {favorite_animal}!")
