0885e8ce200cb32979c059dcc4b6c6d574bd456e
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-04-16 03:08:58 +0500

    Create byvideo.py

---
+++ data/pythonrevision/byvideo.py
@@ -0,0 +1,11 @@
+# List = [] ordered and changeable, Duplicates ok
+
+# Set = { } unordered and immutable, but ADD/Remove Ok... no Duplicates
+
+# tuple = () ordered and immutable, no duplicates, but ADD/Remove not ok
+
+
+# Dictionary = { } unordered and mutable, no duplicates, but ADD/Remove ok
+
+
+
