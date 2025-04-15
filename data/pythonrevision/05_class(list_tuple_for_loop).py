bd13cd31a1d5aae814346d789a48b5b958c275fc
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-04-15 12:50:10 +0500

    1250 14042025

---
+++ data/pythonrevision/05_class(list_tuple_for_loop).py
@@ -0,0 +1,26 @@
+# List
+# iteration operation with loop
+# apply any operation on element
+
+
+#  >>>>>>>>
+
+names : list[str] = ['Sir Zia',"Muhammad Qasim","DR Noman"]
+print(names)
+['Sir Zia', 'Muhammad Qasim', 'DR Noman']
+
+#  >>>>>>>>
+
+names:list[str] = ['Sir Zia', "Muhammad Qasim", "DR Noman"]
+
+for name in names:
+    print(name)
+
+# Sir zia
+# "Muhammad Qasim"
+# DR Noman
+Sir Zia
+Muhammad Qasim
+DR Noman
+
+#  >>>>>>>>
