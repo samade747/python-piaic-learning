fe4a5d84f1ad560dc9178c91ec43649f596221a6
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-04-17 22:01:03 +0500

    Update 10_class(functions).py

--- data/pythonrevision/10_class(functions).py
+++ data/pythonrevision/10_class(functions).py
@@ -249,7 +249,58 @@ print(type(iterator_variable))
 <class 'generator'>
 
 #  >>>>>>>>
+
+for i in iterator_variable:
+    print(i)
+3
+4
+5
+6
+7
+8
+9
+10
+
 #  >>>>>>>>
+
+dir(a)
+['__class__',
+ '__del__',
+ '__delattr__',
+ '__dir__',
+ '__doc__',
+ '__eq__',
+ '__format__',
+ '__ge__',
+ '__getattribute__',
+ '__getstate__',
+ '__gt__',
+ '__hash__',
+ '__init__',
+ '__init_subclass__',
+ '__iter__',
+ '__le__',
+ '__lt__',
+ '__name__',
+ '__ne__',
+ '__new__',
+ '__next__',
+ '__qualname__',
+ '__reduce__',
+ '__reduce_ex__',
+ '__repr__',
+ '__setattr__',
+ '__sizeof__',
+ '__str__',
+ '__subclasshook__',
+ 'close',
+ 'gi_code',
+ 'gi_frame',
+ 'gi_running',
+ 'gi_suspended',
+ 'gi_yieldfrom',
+ 'send',
+ 'throw']
 #  >>>>>>>>
 
 #  >>>>>>>>
