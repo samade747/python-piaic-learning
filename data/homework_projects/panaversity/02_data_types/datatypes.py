4f959eca49f97be72278912284e4908891feb8ca
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-04-21 11:18:04 +0500

    1117

---
+++ data/homework_projects/panaversity/02_data_types/datatypes.py
@@ -0,0 +1,48 @@
+# python data types
+
+# 1 Numeric
+    # a. Integer (int)
+    # b. Floating-Point (float)
+    # c. Complex (complex)
+
+# 2. Boolean (bool)
+    # True or False.
+
+# 3. Sequence Types
+#     a. String (str)
+#     b. List (list)
+#     c. Tuple (tuple)
+#     d. Range (range)
+
+# 4. Set Types
+    # a. Set (set)
+    # b. Frozen Set (frozenset)
+
+# 5. Mapping Type
+    # Dictionary (dict) Stores key-value pairs.
+
+# 6. Binary Types
+    # a. Bytes (bytes)
+    # b. Bytearray (bytearray)
+
+
+# None Data Type in Python
+
+
+
+# Integer (int)
+
+num_int: int = 42
+
+print(type(num_int), "num_int:", num_int)
+
+# output : <class 'int'> num_int: 42
+
+# b. Floating-Point (float)
+# Numbers with decimal points.
+
+num_float: float = 3.14
+
+print(type(num_float), "num_float:", num_float)
+
+# output : <class 'float'> num_float: 3.14
