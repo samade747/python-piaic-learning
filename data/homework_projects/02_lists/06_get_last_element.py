26857a08b04e005408e3646115852b123afe9441
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-04-22 11:00:05 +0500

    Create 06_get_last_element.py

---
+++ data/homework_projects/02_lists/06_get_last_element.py
@@ -0,0 +1,16 @@
+# This line prints the last element of the list by calculating the index manually
+# len(lst) gives the total number of elements, and we subtract 1 to get the last index
+print(list[len(list) - 1])
+
+# This line also prints the last element, but in a shorter and more common way using -1 indexing
+# In Python, lst[-1] always gives the last item in the list
+print(list[-1])
+
+
+
+# 🧠 What You Learned:
+# len(lst) → gives you the length of the list.
+
+# lst[len(lst) - 1] → gets the last element, since Python list indexes start at 0.
+
+# lst[-1] → is the shortcut to get the last item (most Python developers use this!).
