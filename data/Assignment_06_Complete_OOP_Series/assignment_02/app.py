1a04a4002f0b5675a43471c5bca7bda90af2a228
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-05-06 13:01:42 +0500

    Update app.py

---
+++ data/Assignment_06_Complete_OOP_Series/assignment_02/app.py
@@ -0,0 +1,4 @@
+# 2. Using cls
+# Create a class Counter that keeps track of how many objects have been created. 
+# Use a class variable and a class method with cls to manage and display the count.
+
