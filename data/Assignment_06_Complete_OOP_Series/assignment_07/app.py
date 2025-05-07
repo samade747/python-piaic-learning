b0d8e3e9c0fe23f9601e832ad2d9a438f925c55b
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-05-08 00:48:00 +0500

    Update app.py

--- data/Assignment_06_Complete_OOP_Series/assignment_07/app.py
+++ data/Assignment_06_Complete_OOP_Series/assignment_07/app.py
@@ -6,3 +6,27 @@
 # Try accessing all three variables from an object of the class and document what happens.
 
 
+class Employee():
+    def __init__(self, name, salary, ssn):
+        self.name = name # public variable
+        self._salary = salary # protected variable
+        self.__ssn = ssn # private variable
+
+
+emp = Employee("samad", 50000$, "+923328222026")
+
+print(emp.name) # Accessing public variable
+
+# Protected variable: Access technically allowed, lekin convention hai ke direct access na kiya jaye
+print("Salary (_Protected) :",emp._salary)
+
+
+# Private variable: Direct access not allowed, error aayega
+try:
+    print("SSN (__Private) :",emp.__ssn)
+except AttributeError as e:
+    print("Error Acessing Private Variable :",e)
+
+
+# Lekin private variable ko indirectly access kiya ja sakta hai (name mangling ke zariye)
+print("SSN (accessed via name mangling):", emp._Employee__ssn)
