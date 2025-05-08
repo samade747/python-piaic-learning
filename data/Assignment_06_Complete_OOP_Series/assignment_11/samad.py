273e883fffe1c2240dcf7eb8b9cf9048efdd0612
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-05-08 11:22:17 +0500

    Create samad.py

---
+++ data/Assignment_06_Complete_OOP_Series/assignment_11/samad.py
@@ -0,0 +1,29 @@
+# Class Methods
+# Create a class Book with a class variable total_books. Add a class method 
+# increment_book_count() to increase the count when a new book is added.
+
+
+class Book():
+    #Class variabe bnaiya jo sab books ke liye common hai
+    total_books = 0
+
+    def __init__(self, title ):
+        self.title = title
+        Book.increment_book_count()
+
+
+    @classmethod
+    def increment_book_count(cls):
+        cls.total_books += 1 # Class variable ko 1 add hoga
+
+
+
+book1 = Book("py learning")
+book2 = Book("ai learning")
+
+
+# out put
+print(book1.total_books)
+print(book2.total_books)
+
+print("total books" Book.total_books) # class se bhi access kiya ja sakta hai
