b49432e74eaf9715c24b1963818554b2712155b5
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-05-10 00:19:29 +0500

    Create app.py

---
+++ data/Assignment_06_Complete_OOP_Series/assignment_18/app.py
@@ -0,0 +1,3 @@
+# Property Decorators: @property, @setter, and @deleter
+# Create a class Product with a private attribute _price. Use @property to get the price,
+#  @price.setter to update it, and @price.deleter to delete it.
