4b7780b9fb775973eb4b0918a8532485f9fd8531
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-05-10 00:09:07 +0500

    Update samad.py

--- data/Assignment_06_Complete_OOP_Series/assignment_17/samad.py
+++ data/Assignment_06_Complete_OOP_Series/assignment_17/samad.py
@@ -11,3 +11,19 @@ def add_greeting(cls):
 
     cls.greet = greet  # class ko greet method add kar rahe hain
     return cls  # modified class return kar rahe hai    
+
+
+# Person class par decorator apply kiya
+@add_greeting
+class Person:
+    def __init__(self, name):
+        self.name = name  # Har person ka naam set kiyah
+
+# Object create kiya
+p1 = Person("Samad")  # Person class ka object create kiya
+p2 = Person("maryam")  # Person class ka object create kiya
+
+
+# Personalized greeting call ki
+print(p1.greet())  # Output: Hello, Ali! Greeting from Decorator.
+print(p2.greet())  # Output: Hello, Sara! Greeting from Decorator.
